74e679ee0ecb56cfc657a3dae6e39d062bb6f571
SebastienDam01 <s.c.h.dam@amsterdamumc.nl>
2026-01-02 11:59:50 +0100

    FIX tests/matrices directory and .mat files

--- multilayer/tests/load_samples.py
+++ multilayer/tests/load_samples.py
@@ -6,12 +6,13 @@ try:
     TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 except NameError:
     TEST_DIR = os.getcwd()
-    
+MAT_DIR = os.path.join(TEST_DIR, 'matrices')
+
 NODE_NUMBER=15
     
-def test_path(fname):
+def mat_path(fname):
     """
-    Return the full path to a test file located in the test directory.
+    Return the full path to a test file located in the tests/matrices directory.
 
     Parameters
     ----------
@@ -23,14 +24,14 @@ def test_path(fname):
     str
         The absolute path by joining the test directory and the filename.
     """
-    return os.path.join(TEST_DIR, fname)
+    return os.path.join(MAT_DIR, fname)
     
 def save_sample():
     """Save the sub-graph of dimensions NODE_NUMBERxNODE_NUMBER of the first individual of `supra_randmst.mat` in the test folder."""
     data = loadmat("supra_randmst.mat")
     name = list(data.keys())[-1]
     sample = data[name][:NODE_NUMBER, :NODE_NUMBER, 0]
-    savemat(test_path("sample_data.mat"), {
+    savemat(mat_path("sample_data.mat"), {
         "sample": sample
     })
     return
@@ -40,18 +41,18 @@ def save_full_sample():
     data = loadmat("supra_randmst.mat")
     name = list(data.keys())[-1]
     sample = data[name][..., 0]
-    savemat(test_path("full_sample_data.mat"), {
+    savemat(mat_path("full_sample_data.mat"), {
         "sample": sample
     })
     return
 
 def load_sample():
     """Load a truncated sample data from a test file located in the test directory."""
-    return loadmat(test_path("sample_data.mat"))
+    return loadmat(mat_path("sample_data.mat"))
 
 def load_full_sample():
     """Load a full sample data from a test file located in the test directory."""
-    return loadmat(test_path("full_sample_data.mat"))
+    return loadmat(mat_path("full_sample_data.mat"))
 
 def save_chain_graph(n=NODE_NUMBER):
     """
@@ -67,10 +68,10 @@ def save_chain_graph(n=NODE_NUMBER):
         A[i, i + 1] = 1
         A[i + 1, i] = 1
     
-    savemat(test_path("chain_graph.mat"), {"chain_graph": A})
+    savemat(mat_path("chain_graph.mat"), {"chain_graph": A})
     return 
 
 def load_chain_graph(n=NODE_NUMBER):
     """Load a chain graph from a test file located in the test directory."""
-    return loadmat(test_path("chain_graph.mat"))
+    return loadmat(mat_path("chain_graph.mat"))
 
